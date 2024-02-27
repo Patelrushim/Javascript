62aaf2f9f36de9582fbd3adafe339de33d417b25
Patelrushim <94231879+Patelrushim@users.noreply.github.com>
2024-02-27 18:35:26 +0000

    comaprision of data

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log(2 > 1);
+console.log(2 == 2);
+console.log(2 >=1);
+console.log(2 < 1);
+console.log(2 != 1)
+
+
+console.log("2" > 1);
+console.log(null > 1);
+console.log(null == 0);
+
+console.log(undefind > 1);
+console.log(undefind == 0);
+
+// === (strict comparition ir also check the datatype of the data)
+
+console.log("2" === 2);
