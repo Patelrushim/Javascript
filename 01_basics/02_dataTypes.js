a124646e5b9b0c22854b6363ef5e7915166abe58
Patelrushim <94231879+Patelrushim@users.noreply.github.com>
2024-02-27 15:15:39 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) //we are useing nodejs, not browser
+
+console.log(3+3) // code readabiliy should be high
+ console.log("Rushi Patel")
+ 
+let name = "Rushi"
+let age = 19
+let isLoggedId = false
+let state;
+
+// number => 2 to  power 53
+// bigint 
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof"Rushi Patel") // string
+console.log(typeof"null") //object
+
